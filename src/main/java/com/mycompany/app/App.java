1d3b5509ba932882f8244e13f0322da352e3173e
Ivan Smahlenko <ivan_smahlenko@epam.com>
2021-11-01 15:28:59 +0200

    changed sources

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,13 +4,7 @@ package com.mycompany.app;
  * Hello world!
  */
 public class App
-{
-
-    private final String message = "Hello World!";
-
-    public App() {}
-
-    public static void main(String[] args) {
+{String[] args) {
         System.out.println(new App().getMessage());
     }
 
